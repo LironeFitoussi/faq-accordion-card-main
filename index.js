c7834bc69fbdef5fe791f286646ba96d699442d2
LironeFitoussi <lironefit@icloud.com>
2023-08-26 22:38:12 +0300

    Js update

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ var questionBlocks = document.getElementsByClassName("qa-block");
 
 for (var i = 0; i < arrowImages.length; i++) {
   arrowImages[i].addEventListener("click", function() {
-    var arrowImage = this;
-    var question = arrowImage.closest(".qa-block").querySelector(".question");
+    // Check if the clicked arrow is already rotated
+    var isRotated = this.classList.contains("rotated");
 
-    // Toggle the rotated class for arrow
-    arrowImage.classList.toggle("rotated");
-    
-    // Toggle the visible class for answer
-    var answer = arrowImage.closest(".qa-block").querySelector(".answer");
-    answer.classList.toggle("visible");
+    // Reset the state of all arrows, answers, and questions
+    for (var j = 0; j < arrowImages.length; j++) {
+      arrowImages[j].classList.remove("rotated");
+    }
 
-    // Toggle the question-clicked class
-    question.classList.toggle("question-clicked");
+    for (var k = 0; k < questionBlocks.length; k++) {
+      var answer = questionBlocks[k].querySelector(".answer");
+      var question = questionBlocks[k].querySelector(".question");
+      
+      answer.classList.remove("visible");
+      question.classList.remove("question-clicked");
+    }
+
+    // If the clicked arrow was not already rotated, handle the clicked arrow, answer, and question
+    if (!isRotated) {
+      var arrowImage = this;
+      var question = arrowImage.closest(".qa-block").querySelector(".question");
+
+      arrowImage.classList.toggle("rotated");
+      
+      var answer = arrowImage.closest(".qa-block").querySelector(".answer");
+      answer.classList.toggle("visible");
+
+      question.classList.toggle("question-clicked");
+    }
   });
 }
